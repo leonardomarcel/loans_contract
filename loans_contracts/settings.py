27299c055b0e6f6a991e9e9ad249cdf711320af9
Leonardo Carvalho <leonardomarcel@MacBook-Air-de-Leonardo.local>
2019-06-11 21:44:42 -0300

    Deixar visivel a interface do django-rest

--- loans_contracts/settings.py
+++ loans_contracts/settings.py
@@ -52,12 +52,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
 
-     'DEFAULT_RENDERER_CLASSES': (
-         'rest_framework.renderers.JSONRenderer',
-     ),
-     'DEFAULT_PARSER_CLASSES': (
-         'rest_framework.parsers.JSONParser',
-     ),
+    
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
