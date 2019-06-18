bce6e2a06e6bdf83fa8ffdfbbfb2e1f8d062f180
leonardo.carvalho <leonardo.carvalho@itec.al.gov.br>
2019-06-18 10:38:19 -0300

    Ajustes na tela de login

--- loans_contracts/settings.py
+++ loans_contracts/settings.py
@@ -100,9 +100,9 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'loans_contracts',
+        'NAME': 'loans_contract',
         'USER': 'postgres',
-        'PASSWORD': 'postgre',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
